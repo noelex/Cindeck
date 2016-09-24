e2922a7612898e98b7d46a05ce193b4641ac0ab2
noelex <akkarin@live.jp>
2016-09-24 10:16:04 +0800

    バージョンアップ

--- Cindeck.Core/AppConfig.cs
+++ Cindeck.Core/AppConfig.cs
@@ -41,7 +41,7 @@ namespace Cindeck.Core
             NextOid = 1;
         }
 
-        public string Version => "v1.9.1";
+        public string Version => "v1.9.2";
 
         [DataMember(Order = 1)]
         private int NextOid
